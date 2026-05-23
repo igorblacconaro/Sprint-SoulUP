a1bacce8d26b2ec57ec5b31599cffd1c7d2c3923
igorblacconaro <igorbs2910@gmail.com>
2026-05-23 17:14:58 -0300

    fix: Arrumando modo escuro da index.html.

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -13,24 +13,35 @@ let card4 = document.getElementsByTagName("section")[4]
 let p4 = document.getElementsByTagName("p") [5]
 let card5 = document.getElementsByTagName("section")[5]
 let p5 = document.getElementsByTagName("p") [6]
+let h2 = document.getElementsByClassName("header_card") [0]
+let h21 = document.getElementsByClassName("header_card") [1]
+let h22 = document.getElementsByClassName("header_card") [2]
+let h23 = document.getElementsByClassName("header_card") [3]
+let h24 = document.getElementsByClassName("header_card") [4]
+
+
 
 function escuro() {
     div.setAttribute("class", "chamada__txt__escuro");
-    heade.setAttribute("class", "cabecalho__escuro")
-    fundo.setAttribute("class", "body__escuro")
-    titulo.setAttribute("class", "titulo__escuro")
-    paragrafo.setAttribute("class", "paragrafo__escuro")
-        card1.style.backgroundColor="#003033cc"
-    p1.style.color="#fff"
-    card2.style.backgroundColor="#003033cc"
-    p2.style.color="#fff"
-    card3.style.backgroundColor="#003033cc"
-    p3.style.color="#fff"
-    card4.style.backgroundColor="#003033cc"
-    p4.style.color="#fff"
-    card5.style.backgroundColor="#003033cc"
-    p5.style.color="#fff"
-
+    heade.setAttribute("class", "cabecalho__escuro");
+    fundo.setAttribute("class", "body__escuro");
+    titulo.setAttribute("class", "titulo__escuro");
+    paragrafo.setAttribute("class", "paragrafo__escuro");  
+    h2.setAttribute("class", "h2__index__escuro");
+    h21.setAttribute("class", "h2__index__escuro");
+    h22.setAttribute("class", "h2__index__escuro");
+    h23.setAttribute("class", "h2__index__escuro");
+    h24.setAttribute("class", "h2__index__escuro");
+    card1.style.backgroundColor="#003033cc";
+    // p1.style.color="#fff"
+    card2.style.backgroundColor="#003033cc";
+    // p2.style.color="#fff"
+    card3.style.backgroundColor="#003033cc";
+    // p3.style.color="#fff"
+    card4.style.backgroundColor="#003033cc";
+    // p4.style.color="#fff"
+    card5.style.backgroundColor="#003033cc";
+    // p5.style.color="#fff"
 
 }
 
